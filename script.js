12d62db15ba2a55df4234b75a2949e8f10ddfc3c
al-moreno <77991840+al-moreno@users.noreply.github.com>
2021-03-24 18:32:57 -0500

    "last commit"

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 //Declaration of variables based on the html tab
 
 var formEl = document.querySelector('#user-form');
-var cityInputEl = document.querySelectorAll('#button');
-var buttonInput = document.querySelector('#buttonInput');
+var cityInputEl = document.getElementById('buttonInput');
+var buttonInput = document.querySelector('#searchedCities');
 var searchedCityEl = document.querySelector('#searchedCities');
+var cityArray = [];
+var message = document.getElementById('message');
+
+
 console.log('1');
 //functions for listening event
 var formSubmitHandler = function (event) {
     event.preventDefault();
-    var cityName = cityInputEl.value.trim();
+    var cityName = cityInputEl.value;
+    console.log(cityName);
+    cityArray.push(cityName);
+    console.log(cityArray);
 
     if (cityName) {
         getCityInfo(cityName);
 
         buttonInput.textContent = '';
-        cityInputEl = '';
+        cityInputEl.value = '';
     } else {
         alert("Please provide the name of the city who's weather you wish to inquire about");
     }
@@ -23,7 +30,7 @@ var formSubmitHandler = function (event) {
 console.group('2');
 //function for fetch event
 var getCityInfo = function (city) { 
-    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=laredo&appid=8522788feb48b4797ad60e7823e7392b";
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=8522788feb48b4797ad60e7823e7392b";
     console.log('3');
     // 'https://api.github.com/users/' + user + '/repos';
     // "https://api.openweathermap.org/data/2.5/weather?q=houston&appid=8522788feb48b4797ad60e7823e7392b";
@@ -32,6 +39,7 @@ var getCityInfo = function (city) {
             if (response.ok) {
                 response.json()
                     .then(function (data) {
+                        console.log(data);
                         displayCityInfo(data, city);
                     });
             } else {
@@ -45,36 +53,38 @@ var getCityInfo = function (city) {
 console.log('4');
 //function for creation of li 
 var displayCityInfo = function (info, searchTerm) {
-    if (info.length === 0) {
-        buttonInput.textContent = 'No information found.';
-        return;
-    }
+        console.log('info.length');
+        console.log(info);
+        console.log('searchTerm');
+        console.log(searchTerm);
+    
     console.log('5');
     searchedCityEl.textContent = searchTerm;
 
-    for (var i = 0; i < info.length; i++) {
-        var cityNameEl = info[i].something.something + '/' + info[i].something;
+    for (var i = 0; i < cityArray.length; i++) {
+         var cityNameEl = cityArray[i];
+        // console.log(cityNameEl);
 
-        var cityEl = document.createElement('div');
+        var cityElement = document.createElement('div');
         cityElement.classList = 'list-item flex-frow justify-space-between alight-left';
 
         var cityEl2 = document.createElement('span');
-        cityEl2.textContent = cityNameEl;
-        cityEl.appendChild(cityEl2);
+         cityEl2.textContent = cityNameEl;
+        cityElement.appendChild(cityEl2);
         console.log('6');
 
         var statusEl = document.createElement('span');
         statusEl.classList = 'flex-row align-center';
-        if (info[i].open_issues_count > 0) {
-            statusEl.innerHTML =
-                "<i class='fas fa-times status-icon icon-danger'></i>" + info[i].open_issues_count + ' issue(s)';
-        } else {
-            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-        }
+        // if (info[i] > 0) {
+        //     statusEl.innerHTML =
+        //         "<i class='fas fa-times status-icon icon-danger'></i>" + info[i].open_issues_count + ' issue(s)';
+        // } else {
+        //     statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        // }
+        
+        cityElement.appendChild(statusEl);
 
-        cityEl.appendChild(statusEl);
-
-        buttonInput.appendChild(cityEl);
+        buttonInput.appendChild(cityElement);
     }
 console.log('7');
     
